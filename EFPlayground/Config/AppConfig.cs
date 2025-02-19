ff548b42bacc4850581a3d9e85336f8f1d9b1281
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-19 17:49:36 +0200

    feat: add EFPlayground solution

---
+++ EFPlayground/Config/AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Config;
+
+public static class AppConfig
+{
+    private static string _configFileName = "app.settings.json";
+    private static IConfigurationRoot _configObj = new ConfigurationBuilder().AddJsonFile(_configFileName).Build();
+
+    public static string ConnectionString
+    {
+        get => _configObj.GetSection("ConnectionString").Value;
+    }
+}
